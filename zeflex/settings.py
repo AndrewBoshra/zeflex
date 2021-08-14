4960649bea1621b3d551e47e5e11619355a475f4
AndrewBoshra <andrew.boshr@gmail.com>
2021-08-15 01:24:32 +0200

    fix static files

--- zeflex/settings.py
+++ zeflex/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[
-           os.path.join(BASE_DIR , 'build','static')
+           os.path.join(BASE_DIR , 'build/static')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 REST_FRAMEWORK = {
